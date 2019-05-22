3d0938faeadcd43403cb9685fa190f4ae426fa52
Adrian Brand <adrian.brand@humanservices.gov.au>
2019-05-23 09:44:32 +1000

    Clean up PushStack

--- projects/ngx-ez/src/lib/ez-core/rxjs/push-stack.ts
+++ projects/ngx-ez/src/lib/ez-core/rxjs/push-stack.ts
@@ -1,21 +1,15 @@
-import { Observable, BehaviorSubject, Subscription, SubscriptionLike, combineLatest, Subscriber } from 'rxjs';
+import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-export class PushStack<T> extends Observable<T> implements SubscriptionLike {
+export class PushStack<T> extends Observable<T> {
   private behaviorSubject$ = new BehaviorSubject(this.value);
-
-  private observable$ = this.parent$ ?
-    combineLatest(this.behaviorSubject$, this.parent$).pipe(
-      map(([behaviorSubject, parent]) => behaviorSubject || parent || this.defaultValue)
-    ) :
-      this.behaviorSubject$.pipe(map(behaviorSubject => behaviorSubject || this.defaultValue));
-
-  get closed(): boolean {
-    return this.behaviorSubject$.closed;
-  }
 
   constructor(private value: T, private defaultValue: T, private parent$?: Observable<T>) {
     super();
+    this.source = this.parent$ ?
+      combineLatest(this.behaviorSubject$, this.parent$).pipe(
+        map(([behaviorSubject, parent]) => behaviorSubject || parent || this.defaultValue)
+      ) : this.behaviorSubject$.pipe(map(behaviorSubject => behaviorSubject || this.defaultValue));
   }
 
   complete() {
@@ -24,10 +18,6 @@ export class PushStack<T> extends Observable<T> implements SubscriptionLike {
 
   unsubscribe() {
     this.behaviorSubject$.unsubscribe();
-  }
-
-  _subscribe(subscriber: Subscriber<T>): Subscription {
-    return this.observable$.subscribe(subscriber);
   }
 
   next(value: T): void {
