bdf8c8fb935bf6412acdb425582b12f5df2e90b7
Adrian Brand <adrian.brand@humanservices.gov.au>
2019-05-27 15:00:52 +1000

    Remove repeat imports

--- projects/ngx-ez/src/public-api.ts
+++ projects/ngx-ez/src/public-api.ts
@@ -10,6 +10,7 @@ export * from './lib/ez-forms/components/ez-group/ez-group.component';
 export * from './lib/ez-forms/validators/validator-base';
 export * from './lib/ez-forms/directives/ez-form.directive';
 export * from './lib/ez-forms/services/ez-form-config.service';
+
 export * from './lib/ez-core/models/options';
 
 export * from './lib/ez-table/ez-table.module';
@@ -17,8 +18,6 @@ export * from './lib/ez-table/components/ez-table/ez-table.component';
 export * from './lib/ez-table/components/ez-table-search/ez-table-search.component';
 export * from './lib/ez-table/components/ez-table-pager/ez-table-pager.component';
 export * from './lib/ez-table/components/ez-column/ez-column.component';
-export * from './lib/ez-table/ez-table.module';
-export * from './lib/ez-table/ez-table.module';
 
 export * from './lib/ez-core/functions';
 
