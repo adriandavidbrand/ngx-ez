a58e0e914ed7b4730a4ae6aabab829febe11a4dc
Adrian Brand <adrian.brand@humanservices.gov.au>
2019-05-28 11:44:04 +1000

    Add form classes

--- projects/ngx-ez/src/lib/ez-forms/directives/ez-form.directive.spec.ts
+++ projects/ngx-ez/src/lib/ez-forms/directives/ez-form.directive.spec.ts
@@ -2,9 +2,81 @@ import { EzFormDirective } from './ez-form.directive';
 import { NgForm } from '@angular/forms';
 import { ElementRef } from '@angular/core';
 
+import { EzFormConfigService } from '../services/ez-form-config.service';
+
 describe('EzFormDirective', () => {
+  let ngForm: NgForm;
+  let form: ElementRef;
+  let configService: EzFormConfigService;
+
+  beforeEach(() => {
+    ngForm = new NgForm([], []);
+    form = new ElementRef(document.createElement('form'));
+    configService = new EzFormConfigService();
+  });
+
   it('should create an instance', () => {
-    const directive = new EzFormDirective(new NgForm([], []), new ElementRef(document.createElement('form')));
+    const directive = new EzFormDirective(ngForm, form, configService);
     expect(directive).toBeTruthy();
+  });
+
+  it('should add single formClass', () => {
+    configService.formClasses = 'formClass';
+    const directive = new EzFormDirective(ngForm, form, configService);
+    expect(form.nativeElement.classList.contains('formClass')).toBeTruthy();
+  });
+
+  it('should add multiple formClasses from string', () => {
+    configService.formClasses = 'formClass1 formClass2';
+    const directive = new EzFormDirective(ngForm, form, configService);
+    expect(form.nativeElement.classList.contains('formClass1')).toBeTruthy();
+    expect(form.nativeElement.classList.contains('formClass2')).toBeTruthy();
+  });
+
+  it('should add multiple formClasses from array', () => {
+    configService.formClasses = ['formClass1', 'formClass2'];
+    const directive = new EzFormDirective(ngForm, form, configService);
+    expect(form.nativeElement.classList.contains('formClass1')).toBeTruthy();
+    expect(form.nativeElement.classList.contains('formClass2')).toBeTruthy();
+  });
+
+  it('should add single formSubmittedClass on submit', () => {
+    configService.formSubmittedClasses = 'formClass';
+    const directive = new EzFormDirective(ngForm, form, configService);
+    ngForm.ngSubmit.emit();
+    expect(form.nativeElement.classList.contains('formClass')).toBeTruthy();
+  });
+
+  it('should remove single formSubmittedClass on reset', () => {
+    configService.formSubmittedClasses = 'formClass';
+    const directive = new EzFormDirective(ngForm, form, configService);
+    ngForm.ngSubmit.emit();
+    form.nativeElement.reset();
+    expect(form.nativeElement.classList.contains('formClass')).toBeFalsy();
+  });
+
+  it('should add multiple formSubmittedClasses from string on submit', () => {
+    configService.formSubmittedClasses = 'formClass1 formClass2';
+    const directive = new EzFormDirective(ngForm, form, configService);
+    ngForm.ngSubmit.emit();
+    expect(form.nativeElement.classList.contains('formClass1')).toBeTruthy();
+    expect(form.nativeElement.classList.contains('formClass2')).toBeTruthy();
+  });
+
+  it('should add multiple formSubmittedClasses from array on submit', () => {
+    configService.formSubmittedClasses = ['formClass1', 'formClass2'];
+    const directive = new EzFormDirective(ngForm, form, configService);
+    ngForm.ngSubmit.emit();
+    expect(form.nativeElement.classList.contains('formClass1')).toBeTruthy();
+    expect(form.nativeElement.classList.contains('formClass2')).toBeTruthy();
+  });
+
+  it('should remove formSubmittedClasses on reset', () => {
+    configService.formSubmittedClasses = ['formClass1', 'formClass2'];
+    const directive = new EzFormDirective(ngForm, form, configService);
+    ngForm.ngSubmit.emit();
+    form.nativeElement.reset();
+    expect(form.nativeElement.classList.contains('formClass1')).toBeFalsy();
+    expect(form.nativeElement.classList.contains('formClass2')).toBeFalsy();
   });
 });
--- projects/ngx-ez/src/lib/ez-forms/directives/ez-form.directive.ts
+++ projects/ngx-ez/src/lib/ez-forms/directives/ez-form.directive.ts
@@ -2,6 +2,8 @@ import { Directive, ElementRef, OnDestroy, EventEmitter, Input, Output } from '@
 import { NgForm } from '@angular/forms';
 import { Subscription, BehaviorSubject } from 'rxjs';
 
+import { EzFormConfigService } from '../services/ez-form-config.service';
+
 @Directive({
   selector: '[ez-form]'
 })
@@ -22,16 +24,42 @@ export class EzFormDirective implements OnDestroy {
 
   private subscription: Subscription;
 
-  constructor(form: NgForm, private el: ElementRef) {
-    el.nativeElement.classList.add('ez-form');
+  private formSubmittedClasses: string[];
+
+  constructor(form: NgForm, private el: ElementRef, configService: EzFormConfigService) {
+    const elm = el.nativeElement;
+    elm.classList.add('ez-form');
+    if (configService.formClasses) {
+      if (typeof configService.formClasses === 'string') {
+        configService.formClasses.split(' ').forEach(formClass => {
+          elm.classList.add(formClass);
+        });
+      } else if (Array.isArray(configService.formClasses)) {
+        configService.formClasses.forEach(formClass => {
+          elm.classList.add(formClass);
+        });
+      }
+    }
+    if (configService.formSubmittedClasses) {
+      if (typeof configService.formSubmittedClasses === 'string') {
+        this.formSubmittedClasses = configService.formSubmittedClasses.split(' ');
+      } else if (Array.isArray(configService.formSubmittedClasses)) {
+        this.formSubmittedClasses = configService.formSubmittedClasses;
+      }
+    }
     this.subscription = form.ngSubmit.subscribe(() => {
       this.onSubmit(el.nativeElement);
       if (form.valid) {
         this.ezSubmit.emit();
       }
     });
-    el.nativeElement.addEventListener('reset', () => {
-      el.nativeElement.classList.remove('ez-submitted');
+    elm.addEventListener('reset', () => {
+      elm.classList.remove('ez-submitted');
+      if (this.formSubmittedClasses) {
+        this.formSubmittedClasses.forEach(formClass => {
+          elm.classList.remove(formClass);
+        });
+      }
     });
   }
 
@@ -43,6 +71,11 @@ export class EzFormDirective implements OnDestroy {
 
   onSubmit(elm) {
     elm.classList.add('ez-submitted');
+    if (this.formSubmittedClasses) {
+      this.formSubmittedClasses.forEach(formClass => {
+        elm.classList.add(formClass);
+      });
+    }
     const invalid = elm.querySelector(
       '.ng-invalid .ng-invalid input, .ng-invalid .ng-invalid select, .ng-invalid .ng-invalid textarea'
     );
