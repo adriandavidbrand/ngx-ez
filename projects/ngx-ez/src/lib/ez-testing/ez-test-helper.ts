7e916184d7300d28b9c20fa0f12e56865d925cd2
Adrian Brand <adrian.brand@humanservices.gov.au>
2019-07-11 09:34:55 +1000

    Strongly type test helper

--- projects/ngx-ez/src/lib/ez-testing/ez-test-helper.ts
+++ projects/ngx-ez/src/lib/ez-testing/ez-test-helper.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { EzControlBase } from '../ez-forms/components/ez-control-base';
 
 export class EzTestHelper<T> {
   constructor(private fixture: ComponentFixture<T>) {}
 
-  async control(name: string): Promise<any> {
+  async control(name: string): Promise<EzControlBase> {
     this.fixture.detectChanges();
     await this.fixture.whenStable();
     const element = this.fixture.debugElement.query(By.css(`[name=${name}]`));
