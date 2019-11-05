513011cf317ff42790052d1e15bd614986e55b59
Adrian Brand <adriandavidbrand@gmail.com>
2019-11-05 16:27:46 +1000

    Make all falsey vales equal

--- projects/ngx-ez/src/lib/ez-core/functions/natural-sort.ts
+++ projects/ngx-ez/src/lib/ez-core/functions/natural-sort.ts
@@ -1,3 +1,3 @@
 const sort = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' }).compare;
 
-export const naturalSort = (a: any, b: any) => (a === '' ? (b ? 1 : 0) : b === '' ? (a ? -1 : 0) : sort(a, b));
+export const naturalSort = (a: any, b: any) => (!a ? (b ? 1 : 0) : !b ? (a ? -1 : 0) : sort(a, b));
