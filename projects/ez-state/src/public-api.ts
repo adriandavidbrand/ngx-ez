3ba6655459b3c0dd70fcc44965ba4242526d46a5
Adrian Brand <adriandavidbrand@gmail.com>
2020-08-10 16:28:53 +1000

    Add to api

--- projects/ez-state/src/public-api.ts
+++ projects/ez-state/src/public-api.ts
@@ -4,4 +4,6 @@
 
 export * from './lib/ez-state';
 export * from './lib/ez-state-action';
+export * from './lib/ez-cache-base';
 export * from './lib/ez-cache';
+export * from './lib/ez-array-cache';
