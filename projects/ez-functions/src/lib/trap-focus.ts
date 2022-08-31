4f7205c5c05c96faab92407966add5a591a4cdc0
adriandavidbrand <39876791+adriandavidbrand@users.noreply.github.com>
2022-08-31 11:15:43 +0930

    Recalculate tabable elements on each keypress

--- projects/ez-functions/src/lib/trap-focus.ts
+++ projects/ez-functions/src/lib/trap-focus.ts
@@ -1,17 +1,18 @@
 export function trapFocus(element: any) {
-  var focusableEls = element.querySelectorAll(
-    'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]), select:not([disabled])'
-  );
-  var firstFocusableEl = focusableEls[0];
-  var lastFocusableEl = focusableEls[focusableEls.length - 1];
-  var KEYCODE_TAB = 9;
+  const KEYCODE_TAB = 9;
 
   element.addEventListener('keydown', function (e: any) {
-    var isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
+    const isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
 
     if (!isTabPressed) {
       return;
     }
+
+    const focusableEls = element.querySelectorAll(
+      'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]), select:not([disabled])'
+    );
+    const firstFocusableEl = focusableEls[0];
+    const lastFocusableEl = focusableEls[focusableEls.length - 1];
 
     if (e.shiftKey) {
       /* shift + tab */ if (document.activeElement === firstFocusableEl) {
